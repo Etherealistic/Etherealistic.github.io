4e03cc41508eed731f40464de7bdc10d9a324828
Etherealistic <franticfranx@gmail.com>
2024-03-31 18:31:13 +0200

    Fixing CSS

---
+++ src/Components/Profile.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import 'Profile.css';
+import logo from '../images/me.png';
+
+const profileImage = {
+    name: "Francesco Giorcelli",
+};
+
+export default function Profile() {
+    return (
+        <>
+            <div className='imgBox'>
+                <img className="avatar" src={logo} alt={'Photo of ' + profileImage.name} />
+                <div className='titleBox'>
+                    <h1 style={{fontSize:"48px"}}>Francesco Giorcelli</h1>
+                    <h2>Designer and innovator</h2>
+                </div>
+            </div>
+        </>
+    );
+}
