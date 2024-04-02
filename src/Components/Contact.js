f8d3210d182016463ce9b2a1488cc67a9a8d26bc
Etherealistic <franticfranx@gmail.com>
2024-04-02 19:13:26 +0200

    Slight changes to grid behavior

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -7,7 +7,7 @@ export default function Contact() {
         <div className='contactBox'>
             <h1>Contact and Links</h1>
             <p>Email: giorci.francesco@gmail.com</p>
-            <a className='coolButton' href='https://www.youtube.com/@EtherealSandwich/videos' target="_blank"><b>Youtube Channel</b></a>
+            <a className='coolButton' href='https://www.youtube.com/@EtherealSandwich/videos' target="_blank" rel="noreferrer"><b>Youtube Channel</b></a>
         </div>
     )
 }
